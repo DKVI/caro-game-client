2c0277971fb143449baeffb24abcd2cb4971df58
DKVI <92564346+DKVI@users.noreply.github.com>
2023-11-22 15:41:12 +0700

    change API link

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://game-caro-api.vercel.app/api/v1",
 });
 
 export default axiosInstance;
